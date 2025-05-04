4a0ea11049048643df3e821886b174a14c1779f5
phuongtri1909 <tri2003bt@gmail.com>
2025-05-05 00:42:26 +0700

    update

--- run_queue_loop.sh
+++ run_queue_loop.sh
@@ -5,6 +5,6 @@ cd /home/cgshrdaphosting/domains/copier.chitchit.store
 for i in {1..6}
 do
   echo "[`date`] Running queue:work iteration $i" >> storage/logs/queue_loop.log
-  /usr/local/bin/php artisan queue:work --once --stop-when-empty >> storage/logs/queue_loop.log 2>&1
+  /usr/bin/php artisan queue:work --once --stop-when-empty >> storage/logs/queue_loop.log 2>&1
   sleep 10
 done
